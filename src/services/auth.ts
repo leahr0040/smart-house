35ec458374029faac21e4722a4b044a154deab77
Leah <Leahr0040@gmail.com>
2026-06-18 00:21:28 +0300

    feat: add business logic services

---
+++ src/services/auth.ts
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+import { prisma } from '../lib/prisma'
+
+type LoginUserInput = {
+  email: string
+  password: string
+}
+
+export async function loginUser(input: LoginUserInput) {
+  const user = await prisma.user.findUnique({
+    where: { email: input.email }
+  })
+
+  if (!user) {
+    return null
+  }
+
+  const isValid = await bcrypt.compare(input.password, user.password)
+
+  if (!isValid) {
+    return null
+  }
+
+  return user
+}
